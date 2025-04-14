65044beab15d2090833363e28d4c15a018f706ef
Nouman-56 <uniqueboy903@gmail.com>
2025-04-14 05:22:55 +0500

    Create env.js

---
+++ env.js
@@ -0,0 +1,11 @@
+// env.js - KEEP THIS PRIVATE (add to .gitignore)
+window.env = {
+  apiKey: "AIzaSyDrH91_90321shECBZ8mFmTAk86MpBeu6I",
+  authDomain: "battle-clicks.firebaseapp.com",
+  databaseURL: "https://battle-clicks-default-rtdb.firebaseio.com",
+  projectId: "battle-clicks",
+  storageBucket: "battle-clicks.appspot.com",
+  messagingSenderId: "930865479621",
+  appId: "1:930865479621:web:5262af035fe2938740d5ac",
+  measurementId: "G-WSZDE851W3"
+};
